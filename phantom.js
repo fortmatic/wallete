c5c80331ddde53e9cfa2ab985deffa4289b5ebec
iisangil <iisangil@umich.edu>
2020-02-09 16:52:54 -0500

    Initial commmit

---
+++ phantom.js
@@ -0,0 +1,15 @@
+let handleLoginWithMagicLink = (e) => {
+  fmPhantom.loginWithMagicLink({email: getInputValue(e, 'email')}).then((user) => {
+    document.getElementById('status').innerHTML = 'Log in successful!'
+  }).catch((err) => (document.getElementById('status').innerHTML = err));
+  document.getElementById('status').innerHTML = 'Magic Link Sent, Please Check your email'
+};
+
+let handleIsLoggedIn = async () => {
+  alert(await fmPhantom.user.isLoggedIn());
+};
+
+let handleLogout = async () => {
+  await fmPhantom.user.logout();
+  document.getElementById('status').innerHTML = 'Login Status'
+};
