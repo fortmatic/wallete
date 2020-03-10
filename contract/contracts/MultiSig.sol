acd53211d6ea93e91c276e6dbe007229e3eb013e
spvyerra <spvyerra@gmail.com>
2020-03-10 18:12:22 -0400

    Smart contract ready for javascript

--- contract/contracts/MultiSig.sol
+++ contract/contracts/MultiSig.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.5.16;
 
 contract MultiSig {
-    address payable public owner; // Will be the address of who deploys to contract
-
+    address public owner; // Will be the address of who deploys to contract
+    uint256 public nonce;
 
     struct transactionData {
         address payable from;
@@ -11,54 +11,54 @@ contract MultiSig {
         uint256 nonceTrans;
         uint256 threshold;
     }
-    mapping(uint256 => address) private whitelistAdd; // list of addresses added to whitelist
 
-    mapping(uint256 => transactionData) private transactions;
+    address[] public whitelistAdd; // list of addresses added to whitelist
+    uint256 public numWhitlist;
 
-    mapping(bytes32 => mapping(address => bool)) public whiteList; // whitelist of address that can sign transactions
-    uint256 private numWhiteList; // Keep track of num of addresses on whitelist
+    transactionData[] public transactions;
+    transactionData[] public pendingTransactions;
+    uint256 public numPendingTx;
 
     mapping(bytes32 => mapping(address => bool)) public signedList; // list of address that have signed the transaction
-    uint256 private numSigs; // Keeps track of num of signatures on transaction
+    mapping(bytes32 => uint256) numSigs;
 
     // Events to emmit
     event transactionCreated(address, address, uint256, uint256);
-
     event SignedTransact(address);
     event AddedWhiteList(address);
     event transactionOccured(address, uint256);
     event emitHash(bytes32);
 
-    uint256 public nonce = 0;
-    uint256 public threshold;
-    // Mapping to keep track of all message hashes that have been approve by ALL REQUIRED owners
-    mapping(bytes32 => uint256) public signedMessages;
-    mapping(bytes32 => uint256) public dataViaTxHash;
-
     constructor() public {
         // Make the owner who deploys contract
         owner = msg.sender;
-
         // Add to white list for owner to sign
-        numSigs = 0;
-        whitelistAdd[numWhiteList] = owner; // add owner's address as address 0 of whitelist addresses 
-        numWhiteList = 1;
+        numWhitlist = 0;
+        numPendingTx = 0;
+        nonce = 0;
+        addAddress(msg.sender); // add owner's address as address 0 of whitelist addresses
+    }
+
+    function findAddress(address find) internal returns (bool success) {
+        for (uint256 index = 0; index < whitelistAdd.length; index++) {
+            if (whitelistAdd[index] == find) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     // Adds address to whitelist so entity has signing ability
-    function addAddress(address newAddress, bytes32 txHash) public returns (bool success) {
+    function addAddress(address newAddress) public returns (bool success) {
         // Requires to ensure owner adding signers
         require(msg.sender == owner, "Sender not authorized");
-        require(
-            whiteList[txHash][newAddress] == false,
-            "Address is already on whitelist"
-        );
+        require(!findAddress(newAddress), "Already added");
 
         // Give address signing ability and emit event
-        whiteList[txHash][newAddress] = true;
-        whitelistAdd[numWhiteList] = newAddress; // add the new address to list of whitelist addresses 
+        whitelistAdd.push(newAddress); // add the new address to list of whitelist addresses
+        numWhitlist++;
         emit AddedWhiteList(newAddress);
-        ++numWhiteList;
         return true;
     }
 
@@ -66,16 +66,20 @@ contract MultiSig {
         address payable _paymentReciever,
         uint256 _threshold,
         uint256 _amount
-    ) external payable returns (bytes32) {
+    ) external payable returns (uint256) {
         require(msg.value == _amount, "Not enough eth to send transaction");
 
-        transactions[nonce] = transactionData(
+        transactionData memory tmp = transactionData(
             msg.sender,
             _paymentReciever,
             _amount,
             nonce,
             _threshold
         );
+
+        transactions.push(tmp);
+        pendingTransactions.push(tmp);
+        numPendingTx++;
 
         emit transactionCreated(
             msg.sender,
@@ -84,32 +88,37 @@ contract MultiSig {
             _threshold
         );
 
-        bytes32 txHash = keccak256(
-            abi.encode(
-                transactions[nonce].to,
-                transactions[nonce].from,
-                transactions[nonce].amount,
-                transactions[nonce].nonceTrans,
-                transactions[nonce].threshold
-            )
-        );
+        bytes32 txHash = encodeTransaction(nonce);
 
-        signedMessages[txHash] = 0;
-        dataViaTxHash[txHash] = nonce;
-        addAddress(msg.sender, txHash);
-        signTransaction(txHash);
+        numSigs[txHash] = 0;
+        signTransaction(nonce);
 
         nonce++;
         emit emitHash(txHash);
 
-        return txHash;
+        return nonce - 1;
+    }
+
+    function encodeTransaction(uint256 index) internal returns (bytes32) {
+        bytes32 tmp = keccak256(
+            abi.encode(
+                transactions[index].to,
+                transactions[index].from,
+                transactions[index].amount,
+                transactions[index].nonceTrans,
+                transactions[index].threshold
+            )
+        );
+
+        return tmp;
     }
 
     // Msg.sender signs transaction
-    function signTransaction(bytes32 txHash) public returns (bool success) {
+    function signTransaction(uint256 index) public returns (bool success) {
+        bytes32 txHash = encodeTransaction(index);
         // Makes sure sender is on whitelist and has not signed yet
         require(
-            whiteList[txHash][msg.sender] == true,
+            findAddress(msg.sender),
             "This address is not on the whitelist"
         );
         require(
@@ -118,22 +127,30 @@ contract MultiSig {
         );
 
         // Increment num of signatures on transact and approve on signedList
-        ++numSigs;
         signedList[txHash][msg.sender] = true;
-        signedMessages[txHash] += 1;
+        numSigs[txHash] += 1;
         emit SignedTransact(msg.sender);
 
-        return checkStatus(txHash);
+        return checkStatus(index);
     }
 
-    function handlePayment(bytes32 txHash) private returns (bool) {
-        address payable reciever = transactions[dataViaTxHash[txHash]].to;
-        reciever.transfer(transactions[dataViaTxHash[txHash]].amount);
+    function handlePayment(uint256 index) private returns (bool) {
+        address payable reciever = transactions[index].to;
+        reciever.transfer(transactions[index].amount);
 
         emit transactionOccured(
-            transactions[dataViaTxHash[txHash]].to,
-            transactions[dataViaTxHash[txHash]].amount
+            transactions[index].to,
+            transactions[index].amount
         );
+
+        for (uint256 i = index; i < pendingTransactions.length - 1; i++) {
+            pendingTransactions[i] = pendingTransactions[i + 1];
+        }
+
+        delete pendingTransactions[pendingTransactions.length - 1];
+        pendingTransactions.length--;
+        numPendingTx--;
+
         return true;
     }
 
@@ -142,12 +159,10 @@ contract MultiSig {
     }
 
     // Getter for number of entities signed
-    function checkStatus(bytes32 txHash) public payable returns (bool success) {
-        if (
-            signedMessages[txHash] >=
-            transactions[dataViaTxHash[txHash]].threshold
-        ) {
-            return handlePayment(txHash);
+    function checkStatus(uint256 index) public payable returns (bool success) {
+        bytes32 txHash = encodeTransaction(index);
+        if (numSigs[txHash] >= transactions[index].threshold) {
+            return handlePayment(index);
         }
 
         return false;
