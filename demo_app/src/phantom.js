73d29a514a603a38b8849f23dae1d3a64cac4dc0
spvyerra <spvyerra@gmail.com>
2020-03-27 22:41:56 -0400

    Updated deployed contract address

--- demo_app/src/phantom.js
+++ demo_app/src/phantom.js
@@ -5,7 +5,7 @@ import Fortmatic from 'fortmatic';
 const fmPhantom = new Fortmatic.Phantom('pk_test_27AE3AE76B3471E0');
 const web3 = new Web3(fmPhantom.getProvider());
 var contract = new web3.eth.Contract(abi.contractAbi); // need abi of smart contract
-contract.options.address = '0xb21E4D8cDcb6D0a5f61c6dcb9486F590027e569E';
+contract.options.address = '0x737b4D07e54f19810E5b6214A377dD233eCc3E49';
 
 
 let handleLoginWithMagicLink = async () => {
