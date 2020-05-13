d3c8a81a1dad4a41dfd2082eaab2227a31e7f24e
spvyerra <spvyerra@gmail.com>
2020-05-12 20:12:13 -0400

    New contract

--- demo_app/src/constants/constants.js
+++ demo_app/src/constants/constants.js
@@ -1,4 +1,4 @@
 import { Magic } from 'magic-sdk';
 
 export const magic = new Magic('pk_test_77A011678F246927');
-export const contractAddress = '0xf703EE3A38fE097545C6b6b555faf6216584bf91';
+export const contractAddress = '0xd959d927De283A43d36386a43367E2ca57D05104';
