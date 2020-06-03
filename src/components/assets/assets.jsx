e460f8eb54a2bbd6594e5e49754ea818475388bf
iisangil <56006742+iisangil@users.noreply.github.com>
2020-06-03 14:49:00 -0400

    Update assets.jsx

--- src/components/assets/assets.jsx
+++ src/components/assets/assets.jsx
@@ -74,18 +74,6 @@ export default class Assets extends Component {
         return (
             <div className="main">
                 <div className="main-blue-box">
-                    {/* <h1 >On Contract {this.state.contractAddress} </h1> */}
-                    {/* <table id="table">
-                        <thead>
-                            <tr>
-                                <th>Assets</th>
-                                <th>Balance</th>
-                            </tr>
-                        </thead>
-                        <tbody id="list">
-                            {this.state.data}
-                        </tbody>
-                    </table> */}
                     <Card>
                          <DataTable
                             title={"On Contract " + this.state.contractAddress}
@@ -97,17 +85,4 @@ export default class Assets extends Component {
             </div>
         );
     }
-
-    // async getData() {
-    //     const balance = await index.contract.methods.contractBalance().call() / Math.pow(10, 18) + " Eth";
-
-    //     const element = (
-    //         <tr>
-    //             <td>Ether</td>
-    //             <td>{balance}</td>
-    //         </tr>
-    //     );
-
-    //     this.setState({ data: element });
-    // }
 };
